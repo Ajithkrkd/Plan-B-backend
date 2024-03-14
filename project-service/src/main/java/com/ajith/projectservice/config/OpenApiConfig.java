94ba9a882c7da618db46e404f0e10549de5be65d
Ajith krkd <ajith2255iti@gmail.com>
2024-03-14 17:24:39 +0530

    swagger open api implementaion

---
+++ project-service/src/main/java/com/ajith/projectservice/config/OpenApiConfig.java
@@ -0,0 +1,46 @@
+package com.ajith.projectservice.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "AJITH K",
+                        email = "ajith2255iti@gmail.com"
+                ),
+                description = "OpenAPI documentation for User Microservice",
+                title = "OpenAPI specification - Project Microservice",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8082"
+                ),
+
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "BearerAuth"
+                )
+        }
+)
+
+@SecurityScheme(
+        name = "BearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
---
+++ workItem-service/src/test/java/com/ajith/workItemservice/WorkItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajith.workItemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
